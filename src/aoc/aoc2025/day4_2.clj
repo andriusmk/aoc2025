18f13bd79d36a507b533f47f4cf7e3c437494256
Andrius Mažeikis <andriusmk@gmail.com>
2025-12-05 20:38:37 +0200

    Bug fix and cleanup

--- src/aoc/aoc2025/day4_2.clj
+++ src/aoc/aoc2025/day4_2.clj
@@ -18,29 +18,25 @@
 
 (def input-file "data/input-4.txt")
 
-(defn get-occupied [input]
-  (set (for [x (range (count (input 0)))
-             y (range (count input))
-             :when (= \@ (nth (input y) x))]
-         [x y])))
-
 (defn solution [input]
   (let [initial-map (map2d/create-map input)
         neighb-off (for [x (range -1 2)
                          y (range -1 2)
                          :when (not (= x y 0))]
-                     (map2d/pos-id initial-map x y))
+                     [x y])
         valid-pos (fn [m pos]
-                    (let [neighbours (map (comp #(map2d/get m % \.) (partial + pos)) neighb-off)]
-                      (< (count (filter (partial = \@) neighbours)) 4)))]
-    (loop [ans 0 curr-map initial-map]
-      (let [valid-poss (filter (fn [[idx c]] (and (= c \@) (valid-pos curr-map idx))) (map2d/enumerate curr-map))
-            new-map (apply (partial map2d/set-to curr-map \.) (map first valid-poss))]
-        (when (zero? ans)
-          (spit "data/tmp/bad-dbg.txt" (string/join \newline (map (fn [[pos _]]
-                                                                    (let [[x y] (map2d/pos-xy new-map pos)]
-                                                                      (str "line: " y " col: " x)))
-                                                                  valid-poss))))
+                    (let [neighbours (map (comp #(map2d/get m % \.)
+                                                (partial mapv + (map2d/pos-xy m pos)))
+                                          neighb-off)]
+                      (< (count (filter (partial = \@)
+                                        neighbours))
+                         4)))]
+    (loop [ans 0
+           curr-map initial-map]
+      (let [valid-poss (filter (fn [[idx c]] (and (= c \@) (valid-pos curr-map idx)))
+                               (map2d/enumerate curr-map))
+            new-map (apply (partial map2d/set-to curr-map \.)
+                           (map first valid-poss))]
         (if (empty? valid-poss) ans
             (recur (+ ans (count valid-poss)) new-map))))))
 
