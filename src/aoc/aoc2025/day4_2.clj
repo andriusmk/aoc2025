4bc820ed2168e8f546512a60eab691aa6e73fd6f
Andrius Mazeikis <andrius.mazeikis@ext.freseniusmedicalcare.com>
2025-12-05 17:29:10 +0200

    Day 4 refactoring attempt (failed)

--- src/aoc/aoc2025/day4_2.clj
+++ src/aoc/aoc2025/day4_2.clj
@@ -1,6 +1,6 @@
 (ns aoc.aoc2025.day4-2
-  (:require [aoc.core :refer [parse-test read-input]]
-            [clojure.set :as set]))
+  (:require [aoc.core :as aoc]
+            [aoc.map2d :as map2d]))
 
 (def test-input "
 ..@@.@@@@.
@@ -24,21 +24,22 @@
          [x y])))
 
 (defn solution [input]
-  (let [neighb-off (list [-1 -1] [-1 0] [-1 1] [0 1] [1 1] [1 0] [1 -1] [0 -1])
-        neighbours (fn [pos] (set (map #(mapv + pos %) neighb-off)))
-        neighb-cnt (fn [occupied pos] (count (set/intersection (neighbours pos) occupied)))
-        valid? (fn [occupied pos] (< (neighb-cnt occupied pos) 4))
-        find-valid (fn [occupied] (set (filter (partial valid? occupied) occupied)))]
-    (loop [occupied (get-occupied input)
-           ans 0]
-      (let [removable (find-valid occupied)
-            rcnt (count removable)]
-        ; (println rcnt)
-        (if (zero? rcnt) ans
-            (recur (set/difference occupied (set removable)) (+ ans rcnt)))))))
+  (let [initial-map (map2d/create-map input)
+        neighb-off (for [x (range -1 2)
+                         y (range -1 2)
+                         :when (not (= x y 0))]
+                     (map2d/pos-id initial-map x y))
+        valid-pos (fn [m pos]
+                    (let [neighbours (map (comp (partial map2d/get m) (partial + pos)) neighb-off)]
+                      (< (count (filter (partial = \@) neighbours)) 4)))]
+    (loop [ans 0 curr-map initial-map]
+      (let [valid-poss (filter (fn [[idx c]] (and (= c \@) (valid-pos curr-map idx))) (map2d/enumerate curr-map))]
+        (if (empty? valid-poss) ans
+            (recur (+ ans (count valid-poss)) (apply (partial map2d/set-to curr-map \.) (map first valid-poss))))))))
 
 (comment
-  (solution (parse-test test-input)) ; must be 43
-  (solution (read-input input-file))
+  (solution (aoc/parse-test test-input)) ; must be 43
+  (solution (aoc/read-input input-file))
+  (map (partial apply str) (split-at 2 "abcd"))
   ;;
   )
