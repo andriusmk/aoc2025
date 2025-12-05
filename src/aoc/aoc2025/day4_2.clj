c133d4f5febff6d6fb3aca2621fc3b5e98a6448d
Andrius Mažeikis <andriusmk@gmail.com>
2025-12-05 21:51:22 +0200

    Refactored original version is faster

--- src/aoc/aoc2025/day4_2.clj
+++ src/aoc/aoc2025/day4_2.clj
@@ -1,5 +1,6 @@
 (ns aoc.aoc2025.day4-2
-  (:require [aoc.core :refer [parse-test read-input]]
+  (:require [aoc.core :as aoc]
+            [aoc.map2d :as map2d]
             [clojure.set :as set]))
 
 (def test-input "
@@ -17,19 +18,20 @@
 
 (def input-file "data/input-4.txt")
 
-(defn get-occupied [input]
-  (set (for [x (range (count (input 0)))
-             y (range (count input))
-             :when (= \@ (nth (input y) x))]
-         [x y])))
+(defn get-occupied [scene]
+  (set (map first (filter (fn [[_ c]] (= c \@)) (map2d/enumerate scene)))))
 
 (defn solution [input]
-  (let [neighb-off (list [-1 -1] [-1 0] [-1 1] [0 1] [1 1] [1 0] [1 -1] [0 -1])
-        neighbours (fn [pos] (set (map #(mapv + pos %) neighb-off)))
+  (let [scene (map2d/create-map input)
+        neighbours (fn [pos] (let [[ px py] (map2d/pos-xy scene pos)]
+                               (set (for [x (range -1 2)
+                                          y (range -1 2)
+                                          :when (not (= x y 0))]
+                                      (map2d/pos-id scene (+ px x) (+ py y))))))
         neighb-cnt (fn [occupied pos] (count (set/intersection (neighbours pos) occupied)))
         valid? (fn [occupied pos] (< (neighb-cnt occupied pos) 4))
         find-valid (fn [occupied] (set (filter (partial valid? occupied) occupied)))]
-    (loop [occupied (get-occupied input)
+    (loop [occupied (get-occupied scene)
            ans 0]
       (let [removable (find-valid occupied)
             rcnt (count removable)]
@@ -38,7 +40,7 @@
             (recur (set/difference occupied (set removable)) (+ ans rcnt)))))))
 
 (comment
-  (solution (parse-test test-input)) ; must be 43
-  (time (solution (read-input input-file)))
+  (solution (aoc/parse-test test-input)) ; must be 43
+  (time (solution (aoc/read-input input-file)))
   ;;
   )
